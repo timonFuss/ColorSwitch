f6d64cb544ec17d60d36e22f43abbd27958844c5
Anonymer Eintrag <avuxx001@pc15-117.intern.mi.hs-rm.de>
2018-07-04 12:29:32 +0200

    added screen, foce landscape

---
+++ ColorSwitch/StartViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartViewController.swift
+//  ColorSwitch
+//
+//  Created by Anonymer Eintrag on 04.07.18.
+//  Copyright © 2018 hs-rm.de. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
